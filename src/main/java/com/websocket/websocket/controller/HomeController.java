68fca916326ec316e04d066ff0004877e63be98a
Wittano <radoslaw.ratyna@gmail.com>
2020-03-04 11:20:19 +0100

    Added basic web pages with models

--- src/main/java/com/websocket/websocket/controller/HomeController.java
+++ src/main/java/com/websocket/websocket/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.websocket.websocket.controller;
 
+import com.websocket.websocket.models.User;
+import com.websocket.websocket.models.UserRegister;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("user", new User());
         return "index";
+    }
+
+    @RequestMapping("/forget")
+    public String forgetPassword(Model model){
+        model.addAttribute("email", "");
+
+        return "forget";
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model){
+        model.addAttribute("user", new UserRegister());
+
+        return "register";
     }
 }
---
+++ src/main/java/com/websocket/websocket/models/UserRegister.java
@@ -0,0 +1,56 @@
+package com.websocket.websocket.models;
+
+import java.util.Date;
+
+public class UserRegister {
+
+    public enum Gender{
+       Male, Female
+    }
+
+    private String name;
+    private String password;
+    private String email;
+    private Date birthday;
+    private Gender gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
