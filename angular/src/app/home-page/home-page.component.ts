5e5bff437dc7f7cb819cc4f97e32cfd2270d1fa8
Wittano <radoslaw.ratyna@gmail.com>
2020-05-21 22:27:35 +0200

    Added function that allows read value from JWT

--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -1,16 +1,27 @@
-import { Component, AfterViewChecked } from '@angular/core';
+import { Component, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../service/user.service';
+import * as jwtToken from 'jwt-decode';
+import { JwtToken } from '../models/jwt-token';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements AfterViewChecked {
-  constructor(private router: Router, private userService: UserService) {}
+export class HomePageComponent implements AfterViewChecked, AfterViewInit {
+  username: string;
 
-  ngAfterViewChecked(): void {
+  constructor(private router: Router, private userService: UserService) {
+    const token: string = localStorage.getItem('token');
+    const decodeToken: JwtToken = jwtToken<JwtToken>(token);
+
+    this.username = decodeToken.sub;
+  }
+
+  ngAfterViewChecked(): void {}
+
+  ngAfterViewInit() {
     if (localStorage.getItem('token') == null) {
       this.router.navigate(['']);
     }
