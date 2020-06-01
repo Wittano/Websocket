03eee4f81e8dec602fd3364b73206321d7f2a8a0
Wittano <radoslaw.ratyna@gmail.com>
2020-06-01 15:06:04 +0200

    Added startup script for other distro linux outside Debian/Ubuntu
    Alse modfied Dockerfile in Angular folder

--- run.sh
+++ run.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-os_name=$(cat /etc/os-release | grep ID | sed 's/ID=//g' | head -n 1)
-package_manager="apt"
-installation_command="install"
-
 function install_maven(){
 	echo "Install maven"
-	sudo $package_manager $installation_command maven
+	sudo apt install maven
 }
 
 function install_node(){
-		echo "Angular: Install Nodejs"
-
-		if [ $os_name = "ubuntu" ] 
-		then
-			curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-			sudo apt-get install -y nodejs
-		else
-			echo "Install nodejs manually!"
-			exit
-		fi
+	echo "Angular: Install Nodejs"
+	curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+	sudo apt-get install -y nodejs
 }
 
 mvn -v || install_maven
@@ -27,14 +16,14 @@ node -v || install_node
 
 if [ ! -f target/websocket-0.2.jar ]
 then
-	echo "Packing Java Project to .jar file"
+	echo "Create Spring-Boot package"
 	mvn package -DskipTests
 fi
 
 if [ ! -d angular/node_modules ]
 then
 	cd angular
-	echo "Angular: Install unnecessary packages"
+	echo "Install Angular and Nodejs dependences"
 	npm install
 	cd ..
 fi
